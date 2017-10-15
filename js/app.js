baab368ff87bf86b5478b41bcd737fdcc57b978b
Justin Blosch <jblosch@gmail.com>
2017-10-15 08:07:30 -0700

    Limit the row / column input to between 1 and 50

--- js/app.js
+++ js/app.js
@@ -12,15 +12,22 @@ $(function() {
       eraseFG,
       eraseBG;
   let mouseButtonDown = false;
-  const artboard = makeGrid(20, 20);
+  const artboard = makeGrid(20, 20); // makes starter grid
 
-  $("table").append(artboard);
+  $("table").append(artboard); // renders grid to screen
 
+  // Checks if data is already stored. If not, stores and assigns values.
+  // Otherwise, just assigns values.
   if(!localStorage.getItem("rows")) {
     setupStorage();
   } else {
     retrieveStorage();
   }
+
+  /**
+    @description Stores data from the site in localStorage and executes
+    retrieveStorage
+  */
 
   function setupStorage() {
     localStorage.setItem("rows", $("#rows").val());
@@ -36,6 +43,11 @@ $(function() {
 
     retrieveStorage();
   }
+
+  /**
+    @description Assigns the values from localStorage to the relevant
+    features of the site
+  */
 
   function retrieveStorage() {
     rows = localStorage.getItem("rows");
@@ -60,73 +72,15 @@ $(function() {
     $("#eraser").css("background-color", eraseBG);
     $("table").find("*").remove();
     $("table").append(grid);
-
   }
 
-  // EVENT LISTENERS
-  $("#color").on("change", function(e) {
-    setupStorage();
-  });
+  /**
+    @description Creates the rows (<tr>) and columns (<td>) of a table
+    @param {number} rows
+    @param {number} columns
+    @returns {string} table
+  */
 
-  $("table").on("mousedown", "td", function(e){
-     mouseButtonDown = true;
-     if(eraser) {
-       $(e.target).css("background-color", "transparent");
-     } else {
-       $(e.target).css("background-color", "#" + color);
-     }
-  });
-
-  $("table").on("mouseover", "td", function(e) {
-    if(mouseButtonDown) {
-      if(eraser) {
-        $(e.target).css("background-color", "transparent");
-      } else {
-        $(e.target).css("background-color", "#" + color);
-      }
-    }
-  });
-
-  $(document).on("mouseup", function(e){
-     mouseButtonDown = false;
-     setupStorage();
-  });
-
-  $("#paintbrush").on("click", function(e) {
-    eraser = false;
-    $("#paintbrush").css({
-      "background-color" : "rgb(84, 72, 72)",
-      "color" : "#fff"
-    });
-
-    $("#eraser").css({
-      "background-color" : "transparent",
-      "color" : "#000"
-    });
-    setupStorage();
-  });
-
-  $("#eraser").on("click", function(e) {
-    eraser = true;
-    $("#eraser").css({
-      "background-color" : "rgb(84, 72, 72)",
-      "color" : "#fff"
-    });
-
-    $("#paintbrush").css({
-      "background-color" : "transparent",
-      "color" : "#000"
-    });
-    setupStorage();
-  });
-
-  $("#makeGridButton").on("click", function(e) {
-    let tableData = makeGrid($("#rows").val(), $("#columns").val());
-    $("table").append(tableData);
-    setupStorage();
-  });
-
-  // GENERATES GRID
   function makeGrid(rows, columns) {
     let table = "";
 
@@ -141,6 +95,105 @@ $(function() {
     }
     return table;
   }
+
+  /**
+    @description Reassigns row / column numbers if > 50 or < 1
+    @param {number} input - the row or column data
+  */
+
+  function limitNumbers(input) {
+    if(input.val() > 50) {
+      input.val(50);
+    }
+
+    if(input.val() < 1) {
+      input.val(1);
+    }
+  }
+
+
+  /****************
+   EVENT LISTENERS
+  *****************/
+
+  // listens for a change of color
+  $("#color").on("change", function(e) {
+    setupStorage();
+  });
+
+  // allows a change of the the cell background color when the mouse clicks on it
+  $("table").on("mousedown", "td", function(e){
+     mouseButtonDown = true;
+     if(eraser) {
+       $(e.target).css("background-color", "transparent");
+     } else {
+       $(e.target).css("background-color", "#" + color);
+     }
+  });
+
+  // allows the user to drag and draw pixels when mouse button is held down
+  $("table").on("mouseover", "td", function(e) {
+    if(mouseButtonDown) {
+      if(eraser) {
+        $(e.target).css("background-color", "transparent");
+      } else {
+        $(e.target).css("background-color", "#" + color);
+      }
+    }
+  });
+
+  // turns off drag and draw feature when button is not pressed
+  $(document).on("mouseup", function(e){
+     mouseButtonDown = false;
+     setupStorage();
+  });
+
+  // styles paintbrush icon as selected and allows user to draw on canvas
+  $("#paintbrush").on("click", function(e) {
+    eraser = false;
+    $("#paintbrush").css({
+      "background-color" : "rgb(84, 72, 72)",
+      "color" : "#fff"
+    });
+
+    $("#eraser").css({
+      "background-color" : "transparent",
+      "color" : "#000"
+    });
+    setupStorage();
+  });
+
+  // styles eraser icon as selected and allows user to erase pixels
+  $("#eraser").on("click", function(e) {
+    eraser = true;
+    $("#eraser").css({
+      "background-color" : "rgb(84, 72, 72)",
+      "color" : "#fff"
+    });
+
+    $("#paintbrush").css({
+      "background-color" : "transparent",
+      "color" : "#000"
+    });
+    setupStorage();
+  });
+
+  // button used to generate a new grid to draw on
+  $("#makeGridButton").on("click", function(e) {
+
+    limitNumbers($("#rows"));
+    limitNumbers($("#columns"));
+
+    let tableData = makeGrid($("#rows").val(), $("#columns").val());
+    $("table").append(tableData);
+    setupStorage();
+  });
+
+  /***********************
+   END OF EVENT LISTENERS
+  ***********************/
+
+  // tests to see if the paintbrush or eraser should be active
   if($("#paintbrush").css("color") === "rgb(255, 255, 255)") {
     eraser = false;
   } else {
